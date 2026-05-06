592544fd8b15a304adf1318ef4a7ab41e5049fae
jonnald <jhshin927@gmail.com>
2026-05-06 15:38:09 +0900

    remove node_modules

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,5 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://yoursite.com',
-  markdown: {
-    shikiConfig: {
-      theme: 'dracula',
-    },
-  },
+ output: 'static'
 });
